bce30d8aef9501c3f84d0b9a613dbbcfcd9fb853
RR-121 <74299785+RR-121@users.noreply.github.com>
2021-09-22 18:11:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,43 @@
-var form, player, game, database, trex, trunning;
+var form, player, game, database, trex, trunning, bg_gs0;
 var playerCount = 0;
 var gameState = 0;
 
 function preload() {
-// trunning = loadAnimation("trex1.png", "trex2.png", "trex3.png");
+  trunning = loadAnimation("Images/trex1.png", "Images/trex2.png", "Images/trex3.png");///
+  bg_gs0 = loadImage("Images/Background.png");///
 }
 
 function setup() {
-    createCanvas(displayWidth - 10, displayHeight - 140);
+  createCanvas(displayWidth, displayHeight - 130);
 
-    database = firebase.database();
 
-    game = new Game();
-    game.getState();
-    game.start();
+  database = firebase.database();
 
-    // vid = createVideo("Video/abc.mp4");
-    // vid.play();
-    // vid.size(300, 200);
+  game = new Game();
+  game.getState();
+  game.start();
+
+  trex = createSprite(displayWidth / 2, displayHeight / 2, 100, 100);///
+  trex.addAnimation("Running", trunning);///
+  trex.visible = false;///
+
+  // vid = createVideo("Video/abc.mp4");
+  // vid.play();
+  // vid.size(300, 200);
 }
 
 function draw() {
   background(255, 255, 255);
 
+  if (gameState === 0)
+    background(bg_gs0);///
+
   if (playerCount === 4) {
     game.update(1);
+    trex.visible = true;///
+    game.tutorial();///
   }
 
-
-  
   drawSprites();
 }
 
