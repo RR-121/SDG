d532152127e5484c368a496d79b1890b5c1a4bc4
RR-121 <74299785+RR-121@users.noreply.github.com>
2021-09-25 20:55:32 +0530

    Update Player.js

--- JS/Player.js
+++ JS/Player.js
@@ -16,37 +16,7 @@ class Player {
     updateCount(count) {
         database.ref('/').update({
             playerCount: count
-        });class Player {
-    constructor() {
-        this.index = null;
-        this.name = null
-        this.positionX = null;
-        this.positionY = null;
-    }
-
-    getCount() {
-        var playerCountRef = database.ref('playerCount');
-        playerCountRef.on('value', (data) => {
-            playerCount = data.val();
-        })
-    }
-
-    updateCount(count) {
-        database.ref('/').update({
-            playerCount: count
         });
-    }
-
-    update() {
-        var playerIndex = "players/player" + this.index;
-        database.ref(playerIndex).set({
-            name: this.name,
-            index: this.index
-        });
-    }
-
-
-}
     }
 
     update() {
