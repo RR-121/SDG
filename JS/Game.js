75a0328000a229408806d24a7513a1db06ca1154
RR-121 <74299785+RR-121@users.noreply.github.com>
2021-09-29 16:37:55 +0530

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -28,9 +28,19 @@ class Game {
         }
 
         p1 = createSprite(displayWidth / 2 - 100, displayHeight / 2 - 130);
+        p1.addImage(p1Img);
         p2 = createSprite(displayWidth / 2 + 100, displayHeight / 2 - 130);
+        p2.addImage(p2Img);
         p3 = createSprite(displayWidth / 2 - 100, displayHeight / 2);
+        p3.addImage(p3Img);
         p4 = createSprite(displayWidth / 2 + 100, displayHeight / 2);
+        p4.addImage(p4Img);
+        push();
+        p1.scale = 0.5;
+        p2.scale = 0.5;
+        p3.scale = 0.5;
+        p4.scale = 0.5;
+        pop();
         players = [p1, p2, p3, p4];
         p1.visible = false;
         p2.visible = false;
@@ -55,6 +65,15 @@ class Game {
         p2.visible = true;
         p3.visible = true;
         p4.visible = true;
+
+        for(var plr in allPlayers) {
+            index += 1;
+
+            if(index === player.index) {
+                camera.position.x = players[index-1].x;
+                camera.position.y = players[index-1].y;
+            }
+        }
 
         drawSprites();
     }
