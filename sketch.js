ff4f14582b18b400694bf8f14c50056fec16f19a
RR-121 <74299785+RR-121@users.noreply.github.com>
2021-10-05 15:32:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,16 @@ var bg_gs0, vid, p1Img, p2Img, p3Img, p4Img;
 var p1, p2, p3, p4, allPlayers, players;
 var playerCount = 0;
 var gameState = 0;
+var r1_rw, r1_bw, r1_bw2, r1_d, r2_rw, r2_bw, r2_bw2, r2_d, r3_uw, r3_rw, r3_d, r6_ld, r6_rd, r6_lw, r6_uw, r6_rw;
+var stairs_lw, stairs_uw, stairs_bw, stairs_rw1, stairs_rw2, stairs_d, stair1, stair2, stair3, stair4, stair5, stair6;
+var house_lw, house_rw, house_bw, house_uw1, house_uw2, house_uw3, house_uw4, house_md, lab_lw, lab_uw, lab_rw, lab_bw;
+var r5_lw, r5_uw1, r5_d, r5_uw2, r4_lw, r4_bw, r4_bw2, r4_bw3, r4_d, r4_bw4;
+var lab_stairs_lw, lab_stairs_uw, lab_stairs_bw, lab_stairs_rw1, lab_stairs_rw2, lab_stairs_d, lab_stair1, lab_stair2, lab_stair3, lab_stair4, lab_stair5, lab_stair6;
+
 
 function preload() {
   bg_gs0 = loadImage("Images/Background.png");
-  vid = createVideo("Video/xyz.mp4");
+  vid = createVideo("Video/abc.mp4");
   vid.size(displayWidth, displayHeight - 130);
   vid.hide();
   p1Img = loadImage("Images/p1.png");
