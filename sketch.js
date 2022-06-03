11eef472621dc7cf8b650caeb7357f6d433b85d2
RR-121 <74299785+RR-121@users.noreply.github.com>
2022-06-03 19:44:05 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var stairs_lw, stairs_uw, stairs_bw, stairs_rw1, stairs_rw2, stairs_d, stair1, s
 var house_lw, house_rw, house_bw, house_uw1, house_uw2, house_uw3, house_uw4, house_md, lab_lw, lab_uw, lab_rw, lab_bw;
 var r5_lw, r5_uw1, r5_d, r5_uw2, r4_lw, r4_bw, r4_bw2, r4_bw3, r4_d, r4_bw4;
 var lab_stairs_lw, lab_stairs_uw, lab_stairs_bw, lab_stairs_rw1, lab_stairs_rw2, lab_stairs_d, lab_stair1, lab_stair2, lab_stair3, lab_stair4, lab_stair5, lab_stair6;
-var r1_d_open, r1_d2_open, r2_d_open, r3_d_open, r4_d_open, r5_d_open, r6_ld_open, r6_rd_open, md_open;
+var r1_d_open, r1_d2_open, r2_d_open, r3_d_open, r4_d_open, r5_d_open, r6_ld_open, r6_rd_open, md_open, stairs_d_open, lab_stairs_d_open;
 
 // Flooring:
 var labf1, labf2, r1f, r1fImg, r2f, r3f, r5f, r6f, r4f1, r4f2, r4f3, r4f4, r4f5, r4f6, r4f7, r4f8, r4f9, r4f10, r4f11, r4f12, r4f13, r4f14, r4f15, r4f16, r4f17, r4f18, r4f19, r4f20, r4f21, r4f22, r4f23, r4f24, r4f25, r4f26, r4f27, r4f28, r4f29, r4f30, r4f31, r4f32, r4f33, r4f34, r4f35, r4f36, r4f37, r4f38, r4f39, r4f40, r4f41, r4f42, r4f43, r4f44, r4f45, r4f46, r4f47, r4f48, r4f49, r4f50, r4f51, r4f52, r4f53, r4f54, r4f55, r4f56, r4f57, r4f58, r4f59, r4f60, r4f61, r4f62, r4f63, r4f64, r4f65, r4f66, r4f67, r4f68, r4f69, r4f70, r4f71, r4f72, r4f73, r4f74, r4f75, r4f76, r4f77, r4f78, r4f79, r4f80, r4f81, r4f82, r4f83, r4f84, r4f85, r4f86, r4f87, r4f88, r4f89, r4f90, r4f91, r4f92, r4f93, r4f94, r4f95, r4f96, r4f97, r4f98, r4f99, r4f100, r4f101, r4f102, r4f103, r4f104, r4f105, r4f106, r4f107, r4f108, r4f109, r4f110, r4f111, r4f112, r4f113, r4fx, r4fy;
@@ -26,11 +26,11 @@ var tv, tvImg, sofal, sofalImg, sofar, sofarImg, sofab, sofabImg, r5sidetable, r
 var diningt, diningtImg, r6cupboardl, r6cupboardlImg, r6cupboardr, r6cupboardrImg, r6shelf1, r6shelf1Img, r6shelf2, r6shelf2Img;
 var labtbl1, labtbl1Img, labtbl2, labtbl2Img, labtbl3, labtbl3Img, labcupboard, labcupboardImg, labshelf, labshelfImg;
 
-// Crates:
+// Crates, vials, keys:
 var crate1, crate2, crate3, crate4, crateImg;
 var crate1Group, crate2Group, crate3Group, crate4Group;
-
-// var randNum = Math.round(random(1, 4));
+var vial1, vial2, vialImg;
+var skey, mainkey1, mainkey2, mainkey3, skeyImg, mainkeyImg;
 
 p5.disableFriendlyErrors = true;
 
@@ -86,6 +86,9 @@ function preload() {
   labshelfImg = loadImage("Images/Lab Shelf.png");
   labcupboardImg = loadImage("Images/Lab Cupboard.png");
   crateImg = loadImage("Images/Box.png");
+  vialImg = loadImage("Images/Chemical vial.png");
+  skeyImg = loadImage("Images/Silver key.png");
+  mainkeyImg = loadImage("Images/Golden key.png");
 }
 
 function setup() {
@@ -168,7 +171,7 @@ function keyTyped() {
       index += 1;
       if (index === player.index) {
         if (player.index === 1) {
-          if (p1_sr.isTouching(r1_d) || p1_sr.isTouching(r1_d2) || p1_sr.isTouching(r2_d) || p1_sr.isTouching(r3_d) || p1_sr.isTouching(r4_d) || p1_sr.isTouching(r5_d) || p1_sr.isTouching(r6_ld) || p1_sr.isTouching(r6_rd) || p1_sr.isTouching(stairs_d) || p1_sr.isTouching(lab_stairs_d) || p1_sr.isTouching(house_md)) {
+          if (p1_sr.isTouching(r1_d) || p1_sr.isTouching(r1_d2) || p1_sr.isTouching(r2_d) || p1_sr.isTouching(r3_d) || p1_sr.isTouching(r4_d) || p1_sr.isTouching(r5_d) || p1_sr.isTouching(r6_ld) || p1_sr.isTouching(r6_rd) || p1_sr.isTouching(stairs_d) || p1_sr.isTouching(lab_stairs_d)) {
             if (p1_sr.isTouching(r1_d)) {
               if (r1_d_open === false)
                 text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
@@ -256,6 +259,28 @@ function keyTyped() {
                 game.updateR6rdStatus(true, 1050, 1040, 100, 10);
               else if (keyCode === 32 && r6_rd_open === true)
                 game.updateR6rdStatus(false, 1100, 995, 10, 100);
+            }
+            if (p1_sr.isTouching(stairs_d)) {
+              if (stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && stairs_d_open === false)
+                game.updateStairsDStatus(true, -162.5, 486.5, 100, 15);
+              else if (keyCode === 32 && stairs_d_open === true)
+                game.updateStairsDStatus(false, -212.5, 526, 15, 100);
+            }
+            if (p1_sr.isTouching(lab_stairs_d)) {
+              if (lab_stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && lab_stairs_d_open === false)
+                game.updateLabStairsDStatus(true, 3582.5, 648.5, 100, 15);
+              else if (keyCode === 32 && lab_stairs_d_open === true)
+                game.updateLabStairsDStatus(false, 3532.5, 606, 15, 100);
             }
           }
         }
@@ -349,6 +374,28 @@ function keyTyped() {
               else if (keyCode === 32 && r6_rd_open === true)
                 game.updateR6rdStatus(false, 1100, 995, 10, 100);
             }
+            if (p2_sr.isTouching(stairs_d)) {
+              if (stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && stairs_d_open === false)
+                game.updateStairsDStatus(true, -162.5, 486.5, 100, 15);
+              else if (keyCode === 32 && stairs_d_open === true)
+                game.updateStairsDStatus(false, -212.5, 526, 15, 100);
+            }
+            if (p2_sr.isTouching(lab_stairs_d)) {
+              if (lab_stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && lab_stairs_d_open === false)
+                game.updateLabStairsDStatus(true, 3582.5, 648.5, 100, 15);
+              else if (keyCode === 32 && lab_stairs_d_open === true)
+                game.updateLabStairsDStatus(false, 3532.5, 606, 15, 100);
+            }
           }
         }
         else if (player.index === 3) {
@@ -441,6 +488,28 @@ function keyTyped() {
               else if (keyCode === 32 && r6_rd_open === true)
                 game.updateR6rdStatus(false, 1100, 995, 10, 100);
             }
+            if (p3_sr.isTouching(stairs_d)) {
+              if (stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && stairs_d_open === false)
+                game.updateStairsDStatus(true, -162.5, 486.5, 100, 15);
+              else if (keyCode === 32 && stairs_d_open === true)
+                game.updateStairsDStatus(false, -212.5, 526, 15, 100);
+            }
+            if (p3_sr.isTouching(lab_stairs_d)) {
+              if (lab_stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && lab_stairs_d_open === false)
+                game.updateLabStairsDStatus(true, 3582.5, 648.5, 100, 15);
+              else if (keyCode === 32 && lab_stairs_d_open === true)
+                game.updateLabStairsDStatus(false, 3532.5, 606, 15, 100);
+            }
           }
         }
         else if (player.index === 4) {
@@ -532,6 +601,28 @@ function keyTyped() {
                 game.updateR6rdStatus(true, 1050, 1040, 100, 10);
               else if (keyCode === 32 && r6_rd_open === true)
                 game.updateR6rdStatus(false, 1100, 995, 10, 100);
+            }
+            if (p4_sr.isTouching(stairs_d)) {
+              if (stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && stairs_d_open === false)
+                game.updateStairsDStatus(true, -162.5, 486.5, 100, 15);
+              else if (keyCode === 32 && stairs_d_open === true)
+                game.updateStairsDStatus(false, -212.5, 526, 15, 100);
+            }
+            if (p4_sr.isTouching(lab_stairs_d)) {
+              if (lab_stairs_d_open === false)
+                text("Press 'Space' to open the door", players[index - 1].x - 70, players[index - 1].y + 50);
+              else
+                text("Press 'Space' to close the door", players[index - 1].x - 70, players[index - 1].y + 50);
+
+              if (keyCode === 32 && lab_stairs_d_open === false)
+                game.updateLabStairsDStatus(true, 3582.5, 648.5, 100, 15);
+              else if (keyCode === 32 && lab_stairs_d_open === true)
+                game.updateLabStairsDStatus(false, 3532.5, 606, 15, 100);
             }
           }
         }
